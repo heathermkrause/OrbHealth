603b7241c4f0a30221243a32a3d775309abdf9fa
Heather Krause <heatherkrause@Heathers-MacBook-Pro.local>
2020-03-17 09:30:16 -0400

    preds

--- OrbHealthDataMerge.R
+++ OrbHealthDataMerge.R
@@ -12,14 +12,20 @@ data3$year <- data3$Year
 data13 <- merge(data1,data3,by=c("Country"),all=T)
 data13b <- merge(data1,data3,by=c("Country","year"),all=T)
 
-fit1 <- lm(value_Under.five.mortality.rate~year+value_Nurses.and.midwives+value_Physicians,data=data1)
+dataF <- data13b[which(data13b$year>1980),]
+
+fit1 <- lm(value_Under.five.mortality.rate~year+value_Nurses.and.midwives+value_Physicians,data=dataF)
 fit1b <- lm(value_Under.five.mortality.rate~year+value_Nurses.and.midwives+value_Physicians,data=data13)
 fit1b2 <- lm(value_Under.five.mortality.rate~year+value_Nurses.and.midwives+value_Physicians,data=data13b)
-fit2 <- lm(value_Under.five.mortality.rate~year+value_Hospital.beds,data=data13b)
-fit3 <- lm(value_Under.five.mortality.rate~year+value_Nurses.and.midwives+value_Physicians+value_Hospital.beds,data=data13b)
-fit4 <- lm(value_Under.five.mortality.rate~year+Health.expenditure.per.capita..PPP..constant.2011.international.....constant.2011.international...,data=data13b)
-fit5 <- lm(value_Under.five.mortality.rate~year+value_Nurses.and.midwives+value_Physicians+Health.expenditure.per.capita..PPP..constant.2011.international.....constant.2011.international...,data=data13b)
+fit2 <- lm(value_Under.five.mortality.rate~year+value_Hospital.beds,data=dataF)
+fit3 <- lm(value_Under.five.mortality.rate~year+value_Nurses.and.midwives+value_Physicians+value_Hospital.beds,data=dataF)
+fit4 <- lm(value_Under.five.mortality.rate~year+Health.expenditure.per.capita..PPP..constant.2011.international.....constant.2011.international...,data=dataF)
+fit5 <- lm(value_Under.five.mortality.rate~year+value_Nurses.and.midwives+value_Physicians+Health.expenditure.per.capita..PPP..constant.2011.international.....constant.2011.international...,data=dataF)
+dataF$Pred1 <- predict(fit1,newdata=dataF)
+dataF$Pred2 <- predict(fit2,newdata=dataF)
+dataF$Pred3 <- predict(fit3,newdata=dataF)
+dataF$Pred4 <- predict(fit4,newdata=dataF)
+dataF$Pred5 <- predict(fit5,newdata=dataF)
 
-
-write.csv(data24, "/Users/heatherkrause/Dropbox/Active Projects 2020/Chamjari/Capital One/DataFailure.csv")
+write.csv(dataF, "/Users/heatherkrause/Dropbox/Active Projects 2020/Orb/Health Worker Data + Research/Heather Orb Health Data Using/PREDS + ORB Mortalitty and Healthcare MERGED Final.csv")
 write.csv(data123, "/Users/heatherkrause/Dropbox/Active Projects 2020/Chamjari/Capital One/Data123.csv")
